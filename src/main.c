9f22dc7497bd64e3f01fcf677516c1d04944a2ef
JSM <47127982+jmac97@users.noreply.github.com>
2025-08-31 12:30:37 -0400

    Bare bones main with plan for later

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// Get input JSON file
+// Get input for start and end positions
+// Parse/determine walkable and not walkable areas - lib?
+// Print starting map
+// ~Path finding algo~ - what are the options out there
+// Print map with path to traverse
+
+int main(void)
+{
+    return 0;
+}
